7a5304289f4cde6bd04ad8be2342bd2ef7c64bcd
jayakoki <40388752+jayakoki@users.noreply.github.com>
2018-06-30 07:15:28 +0000

    Update largest among three numbers.c

--- largest among three numbers.c
+++ largest among three numbers.c
@@ -1,14 +1,42 @@
-#include<stdio.h>
-int main()
+
+#include <stdio.h>
+void main()
 {
-int n;
-printf("enter the number");
-scanf("%d",&n);
-if(a>b>c)
-  printf("a is greater");
- elseif(b>a>c)
-  printf("b is greater");
- else
-  printf("c is greater");
- return o;
- }
+int num1, num2, num3;
+printf("Enter the values of num1, num2 and num3\n");
+
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    printf("num1 = %d\tnum2 = %d\tnum3 = %d\n", num1, num2, num3);
+
+    if (num1 > num2)
+
+    {
+
+        if (num1 > num3)
+
+        {
+
+            printf("num1 is the greatest among three \n");
+
+        }
+
+        else
+
+        {
+
+            printf("num3 is the greatest among three \n");
+
+        }
+
+    }
+
+    else if (num2 > num3)
+
+        printf("num2 is the greatest among three \n");
+
+    else
+
+        printf("num3 is the greatest among three \n");
+
+}
